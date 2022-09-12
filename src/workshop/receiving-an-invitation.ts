ac61a8c4424e3176da20746b98a166a29b646fbf
blu3beri <berend@animo.id>
2022-09-12 15:21:26 +0200

    cleaned up for demo
    Signed-off-by: blu3beri <berend@animo.id>

--- src/workshop/receiving-an-invitation.ts
+++ src/workshop/receiving-an-invitation.ts
@@ -1,12 +1,8 @@
-import { Agent, OutOfBandInvitation } from '@aries-framework/core'
+import type { Agent, OutOfBandInvitation } from '@aries-framework/core'
 
-export async function acceptInvitation(agent: Agent, data: string) {
-  await agent.oob.receiveInvitationFromUrl(data, { reuseConnection: true })
-}
+export async function acceptInvitation(agent: Agent, data: string) {}
 
 export async function parseInvitation(
   agent: Agent,
   data: string
-): Promise<OutOfBandInvitation> {
-  return await agent.oob.parseInvitationShortUrl(data)
-}
+): Promise<OutOfBandInvitation> {}
--- src/workshop/sharing-a-proof.ts
+++ src/workshop/sharing-a-proof.ts
@@ -1,18 +1,7 @@
-import { Agent } from '@aries-framework/core'
+import type { Agent } from '@aries-framework/core'
 
-export async function acceptProof(agent: Agent, proofRecordId: string) {
-  const creds = await agent.proofs.getRequestedCredentialsForProofRequest(
-    proofRecordId
-  )
-  const requestedCredentials =
-    agent.proofs.autoSelectCredentialsForProofRequest(creds)
-  await agent.proofs.acceptRequest(proofRecordId, requestedCredentials)
-}
+export async function acceptProof(agent: Agent, proofRecordId: string) {}
 
-export async function declineProof(agent: Agent, proofRecordId: string) {
-  await agent.proofs.declineRequest(proofRecordId)
-}
+export async function declineProof(agent: Agent, proofRecordId: string) {}
 
-export async function deleteProof(agent: Agent, proofRecordId: string) {
-  await agent.proofs.deleteById(proofRecordId)
-}
+export async function deleteProof(agent: Agent, proofRecordId: string) {}
