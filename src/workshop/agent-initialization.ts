95e9ccf0044ffb05cc54a34854633710122cfe19
blu3beri <berend@animo.id>
2022-09-13 12:44:51 +0200

    seperated the mediator url
    Signed-off-by: blu3beri <berend@animo.id>

--- src/workshop/agent-initialization.ts
+++ src/workshop/agent-initialization.ts
@@ -10,7 +10,10 @@ import {
   WsOutboundTransport,
 } from '@aries-framework/core'
 import { agentDependencies } from '@aries-framework/react-native'
-import { GENESIS_BCORVIN_TEST_NET } from '../ledgers'
+import { pool_transactions_indicio_testnet_genesis } from '../ledgers'
+
+export const mediatorInvitation =
+  'https://http.mediator.community.animo.id?c_i=eyJAdHlwZSI6ICJkaWQ6c292OkJ6Q2JzTlloTXJqSGlxWkRUVUFTSGc7c3BlYy9jb25uZWN0aW9ucy8xLjAvaW52aXRhdGlvbiIsICJAaWQiOiAiYTY1NTgwMTEtZjkyOS00NzY2LTllYzUtZDJkNzI1ZGQ3NmViIiwgImxhYmVsIjogIkFuaW1vIENvbW11bml0eSBNZWRpYXRvciIsICJyZWNpcGllbnRLZXlzIjogWyI4Vm13OGZicmdlaXFHUEpCeVNzR2hxcjU5QTlzMjRBN3lvYUpzVDNyTnFjayJdLCAic2VydmljZUVuZHBvaW50IjogImh0dHBzOi8vaHR0cC5tZWRpYXRvci5jb21tdW5pdHkuYW5pbW8uaWQifQ=='
 
 export async function initializeAgent(): Promise<Agent> {
   const config: InitConfig = {
@@ -26,15 +29,14 @@ export async function initializeAgent(): Promise<Agent> {
 
     indyLedgers: [
       {
-        id: 'genesis-bcovrin-test-net',
+        id: 'indicio-test-net',
         isProduction: false,
-        genesisTransactions: GENESIS_BCORVIN_TEST_NET,
+        genesisTransactions: pool_transactions_indicio_testnet_genesis,
       },
     ],
 
     mediatorPickupStrategy: MediatorPickupStrategy.Implicit,
-    mediatorConnectionsInvite:
-      'https://http.mediator.community.animo.id?c_i=eyJAdHlwZSI6ICJkaWQ6c292OkJ6Q2JzTlloTXJqSGlxWkRUVUFTSGc7c3BlYy9jb25uZWN0aW9ucy8xLjAvaW52aXRhdGlvbiIsICJAaWQiOiAiYTY1NTgwMTEtZjkyOS00NzY2LTllYzUtZDJkNzI1ZGQ3NmViIiwgImxhYmVsIjogIkFuaW1vIENvbW11bml0eSBNZWRpYXRvciIsICJyZWNpcGllbnRLZXlzIjogWyI4Vm13OGZicmdlaXFHUEpCeVNzR2hxcjU5QTlzMjRBN3lvYUpzVDNyTnFjayJdLCAic2VydmljZUVuZHBvaW50IjogImh0dHBzOi8vaHR0cC5tZWRpYXRvci5jb21tdW5pdHkuYW5pbW8uaWQifQ==',
+    mediatorConnectionsInvite: mediatorInvitation,
   }
 
   const agent = new Agent(config, agentDependencies)
