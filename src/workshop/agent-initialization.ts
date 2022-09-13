6b2d7568513d574bf593329e564d503432a5b1c2
blu3beri <berend@animo.id>
2022-09-13 14:51:58 +0200

    extrated mediator invitation
    Signed-off-by: blu3beri <berend@animo.id>

--- src/workshop/agent-initialization.ts
+++ src/workshop/agent-initialization.ts
@@ -1,5 +1,8 @@
 import { Agent } from '@aries-framework/core'
 
+export const mediatorInvitation =
+  'https://http.mediator.community.animo.id?c_i=eyJAdHlwZSI6ICJkaWQ6c292OkJ6Q2JzTlloTXJqSGlxWkRUVUFTSGc7c3BlYy9jb25uZWN0aW9ucy8xLjAvaW52aXRhdGlvbiIsICJAaWQiOiAiYTY1NTgwMTEtZjkyOS00NzY2LTllYzUtZDJkNzI1ZGQ3NmViIiwgImxhYmVsIjogIkFuaW1vIENvbW11bml0eSBNZWRpYXRvciIsICJyZWNpcGllbnRLZXlzIjogWyI4Vm13OGZicmdlaXFHUEpCeVNzR2hxcjU5QTlzMjRBN3lvYUpzVDNyTnFjayJdLCAic2VydmljZUVuZHBvaW50IjogImh0dHBzOi8vaHR0cC5tZWRpYXRvci5jb21tdW5pdHkuYW5pbW8uaWQifQ=='
+
 // In order to get started we must initialize our agent. This agent needs some
 // platform specific dependencies and a configuration.
 export async function initializeAgent(): Promise<Agent> {}
